b5c54d531b89dbbebfca5634682e247421ed7b8c
Christian Rocha <christian@rocha.is>
2021-04-06 15:12:44 -0400

    Don't enable ANSI in cmd.exe until absolutely necessary

--- ansi_windows.go
+++ ansi_windows.go
@@ -4,18 +4,23 @@ package lipgloss
 
 import (
 	"os"
+	"sync"
 
 	"golang.org/x/sys/windows"
 )
+
+var enableANSI sync.Once
 
 // enableANSIColors enables support for ANSI color sequences in the Windows
 // default console (cmd.exe and the PowerShell application). Note that this
 // only works with Windows 10. Also note that Windows Terminal supports colors
 // by default.
-func enableANSIColors() {
-	stdout := windows.Handle(os.Stdout.Fd())
-	var originalMode uint32
+func enableLegacyWindowsANSI() {
+	enableANSI.Do(func() {
+		stdout := windows.Handle(os.Stdout.Fd())
+		var originalMode uint32
 
-	windows.GetConsoleMode(stdout, &originalMode)
-	windows.SetConsoleMode(stdout, originalMode|windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+		windows.GetConsoleMode(stdout, &originalMode)
+		windows.SetConsoleMode(stdout, originalMode|windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+	})
 }
