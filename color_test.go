141bb088c9c0b4e91009ac8c10c8af7bbb6765b4
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-06-01 10:38:18 -0300

    test: simplify & improve output
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- color_test.go
+++ color_test.go
@@ -8,46 +8,47 @@ import (
 )
 
 func TestSetColorProfile(t *testing.T) {
+	input := "hello"
+
 	tt := []struct {
+		name     string
 		profile  termenv.Profile
-		input    string
-		style    Style
 		expected string
 	}{
 		{
+			"ascii",
 			termenv.Ascii,
-			"hello",
-			NewStyle().Foreground(Color("#5A56E0")),
 			"hello",
 		},
 		{
+			"ansi",
 			termenv.ANSI,
-			"hello",
-			NewStyle().Foreground(Color("#5A56E0")),
 			"\x1b[94mhello\x1b[0m",
 		},
 		{
+			"ansi256",
 			termenv.ANSI256,
-			"hello",
-			NewStyle().Foreground(Color("#5A56E0")),
 			"\x1b[38;5;62mhello\x1b[0m",
 		},
 		{
+			"truecolor",
 			termenv.TrueColor,
-			"hello",
-			NewStyle().Foreground(Color("#5A56E0")),
 			"\x1b[38;2;89;86;224mhello\x1b[0m",
 		},
 	}
 
-	for i, tc := range tt {
-		SetColorProfile(tc.profile)
-		res := tc.style.Render(tc.input)
-		if res != tc.expected {
-			t.Errorf("Test %d, expected:\n\n`%s`\n`%s`\n\nActual output:\n\n`%s`\n`%s`\n\n",
-				i, tc.expected, formatEscapes(tc.expected),
-				res, formatEscapes(res))
-		}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			SetColorProfile(tc.profile)
+			style := NewStyle().Foreground(Color("#5A56E0"))
+			res := style.Render(input)
+
+			if res != tc.expected {
+				t.Errorf("Expected:\n\n`%s`\n`%s`\n\nActual output:\n\n`%s`\n`%s`\n\n",
+					tc.expected, formatEscapes(tc.expected),
+					res, formatEscapes(res))
+			}
+		})
 	}
 }
 
