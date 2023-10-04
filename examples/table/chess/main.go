2687d820831879f66068e5db0c7b8946abb599e8
Maas Lalani <maas@lalani.dev>
2023-10-03 23:39:58 -0400

    feat: chess table with table renderer

---
+++ examples/table/chess/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss/table"
+)
+
+func main() {
+	re := lipgloss.NewRenderer(os.Stdout)
+	labelStyle := re.NewStyle().Foreground(lipgloss.Color("241"))
+
+	board := [][]any{
+		{"♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"},
+		{"♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟"},
+		{" ", " ", " ", " ", " ", " ", " ", " "},
+		{" ", " ", " ", " ", " ", " ", " ", " "},
+		{" ", " ", " ", " ", " ", " ", " ", " "},
+		{" ", " ", " ", " ", " ", " ", " ", " "},
+		{"♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙"},
+		{"♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"},
+	}
+
+	t := table.New().
+		Border(lipgloss.NormalBorder()).
+		BorderRow(true).
+		BorderColumn(true).
+		Rows(board...).
+		StyleFunc(func(row, col int) lipgloss.Style {
+			return lipgloss.NewStyle().Padding(0, 1)
+		})
+
+	ranks := labelStyle.Render(strings.Join([]string{" A", "B", "C", "D", "E", "F", "G", "H  "}, "   "))
+	files := labelStyle.Render(strings.Join([]string{" 1", "2", "3", "4", "5", "6", "7", "8 "}, "\n\n "))
+
+	fmt.Println(lipgloss.JoinVertical(lipgloss.Right, lipgloss.JoinHorizontal(lipgloss.Center, files, t.Render()), ranks) + "\n")
+}
