a9f7bc29b07d0b6b3dd1e6fbcf0d1d657a78b221
Maas Lalani <maas@lalani.dev>
2024-05-15 11:05:25 -0400

    refactor: higher level enumerator

--- examples/list/duckduckgoose/main.go
+++ examples/list/duckduckgoose/main.go
@@ -7,17 +7,19 @@ import (
 	"github.com/charmbracelet/lipgloss/list"
 )
 
-func main() {
-	l := list.New("Duck", "Duck", "Duck", "Duck", "Goose", "Duck", "Duck")
-
-	var DuckDuckGooseEnumerator = func(i int) string {
+func DuckDuckGooseEnumerator(l list.List) list.Enumerator {
+	return func(i int) string {
 		if l.At(i) == "Goose" {
 			return "Honk →"
 		}
 		return ""
 	}
+}
 
-	l = l.Enumerator(DuckDuckGooseEnumerator).EnumeratorStyle(lipgloss.NewStyle().Foreground(lipgloss.Color("48")).MarginRight(1))
+func main() {
+	l := list.New("Duck", "Duck", "Duck", "Duck", "Goose", "Duck", "Duck")
+
+	l = l.Enumerator(DuckDuckGooseEnumerator(l)).EnumeratorStyle(lipgloss.NewStyle().Foreground(lipgloss.Color("48")).MarginRight(1))
 
 	fmt.Println(l)
 }
