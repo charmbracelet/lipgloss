ec22a066e70bb0c4db873c288d572005b85cefcd
Maas Lalani <maas@lalani.dev>
2024-05-29 16:23:16 -0400

    docs: add comments

--- tree/enumerator.go
+++ tree/enumerator.go
@@ -1,7 +1,25 @@
 package tree
 
+// Enumerator enumerates a tree. Typically, this is used to draw the branches
+// for the tree nodes and is different for the last child.
+//
+// For example, the default enumerator would be:
+//
+//	func TreeEnumerator(children Children, index int) string {
+//		if children.Length()-1 == index {
+//			return "└──"
+//		}
+//
+//		return "├──"
+//	}
 type Enumerator func(children Children, index int) string
 
+// DefaultEnumerator enumerates a tree.
+//
+// ├── Foo
+// ├── Bar
+// ├── Baz
+// └── Qux
 func DefaultEnumerator(children Children, index int) string {
 	if children.Length()-1 == index {
 		return "└──"
@@ -9,6 +27,12 @@ func DefaultEnumerator(children Children, index int) string {
 	return "├──"
 }
 
+// RoundedEnumerator enumerates a tree with rounded edges.
+//
+// ├── Foo
+// ├── Bar
+// ├── Baz
+// ╰── Qux
 func RoundedEnumerator(children Children, index int) string {
 	if children.Length()-1 == index {
 		return "╰──"
@@ -16,8 +40,32 @@ func RoundedEnumerator(children Children, index int) string {
 	return "├──"
 }
 
+// Indenter indents the children of a tree.
+//
+// Indenters allow for displaying nested tree items with connecting borders
+// to sibling nodes.
+//
+// For example, the default indenter would be:
+//
+//	func TreeIndenter(children Children, index int) string {
+//		if children.Length()-1 == index {
+//			return "│  "
+//		}
+//
+//		return "   "
+//	}
 type Indenter func(children Children, index int) string
 
+// DefaultIndenter indents a tree for nested trees and multiline content.
+//
+// ├── Foo
+// ├── Bar
+// │   ├── Qux
+// │   ├── Quux
+// │   │   ├── Foo
+// │   │   └── Bar
+// │   └── Quuux
+// └── Baz
 func DefaultIndenter(children Children, index int) string {
 	if children.Length()-1 == index {
 		return "   "
