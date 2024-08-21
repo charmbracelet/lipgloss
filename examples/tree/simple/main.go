9e81121df3f1514d4dd6117d8039823c74395b70
Christian Rocha <christian@rocha.is>
2024-08-21 17:18:16 -0400

    docs(examples): update tree examples to (mostly) match readme

--- examples/tree/simple/main.go
+++ examples/tree/simple/main.go
@@ -8,19 +8,19 @@ import (
 
 func main() {
 	t := tree.Root(".").
-		Child("Item 1").
+		Child("macOS").
 		Child(
 			tree.New().
-				Root("Item 2").
-				Child("Item 2.1").
-				Child("Item 2.2").
-				Child("Item 2.3"),
+				Root("Linux").
+				Child("NixOS").
+				Child("Arch Linux (btw)").
+				Child("Void Linux"),
 		).
 		Child(
 			tree.New().
-				Root("Item 3").
-				Child("Item 3.1").
-				Child("Item 3.2"),
+				Root("BSD").
+				Child("FreeBSD").
+				Child("OpenBSD"),
 		)
 
 	fmt.Println(t)
