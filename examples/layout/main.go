e621cb8e897bce84cbbbf651e5c1017139ca3bf4
bashbunni <bunni@bashbunni.dev>
2024-09-24 11:08:02 -0700

    refactor: change blended colours

--- examples/layout/main.go
+++ examples/layout/main.go
@@ -32,7 +32,7 @@ var (
 	subtle    = lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#383838"}
 	highlight = lipgloss.AdaptiveColor{Light: "#874BFD", Dark: "#7D56F4"}
 	special   = lipgloss.AdaptiveColor{Light: "#43BF6D", Dark: "#73F59F"}
-	blends    = gamut.Blends(highlight, special, 24)
+	blends    = gamut.Blends(lipgloss.Color("#F25D94"), lipgloss.Color("#EDFF82"), 50)
 
 	divider = lipgloss.NewStyle().
 		SetString("•").
