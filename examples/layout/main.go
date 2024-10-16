805e5a4863efbb7b042d6855551a3538d53d2d5b
Christian Rocha <christian@rocha.is>
2024-10-16 17:12:07 -0400

    docs(examples): fix background detection in layout example

--- examples/layout/main.go
+++ examples/layout/main.go
@@ -24,7 +24,14 @@ const (
 	columnWidth = 30
 )
 
-var hasDarkBG bool
+var (
+	// Whether the detected background color is dark. We detect this in init().
+	hasDarkBG bool
+
+	// A helper function for choosing either a light or dark color based on the
+	// detected background color. We create this in init().
+	adaptive lipgloss.Adapt
+)
 
 func init() {
 	var err error
@@ -35,185 +42,182 @@ func init() {
 		fmt.Fprintf(os.Stderr, "Could not detect background color: %v\n", err)
 		os.Exit(1)
 	}
+
+	// Create a new helper function for choosing either a light or dark color
+	// based on the detected background color.
+	adaptive = lipgloss.Adapt(hasDarkBG)
 }
 
-// Create a new helper function for choosing either a light or dark color based
-// on the detected background color.
-//
-// lipgloss.Adapt returns a function whose signature looks like this:
-//
-//	func (lightColor, darkColor string) chosenColor string
-var adaptive = lipgloss.Adapt(hasDarkBG)
+func main() {
 
-// Style definitions.
-var (
+	// Style definitions.
+	var (
 
-	// General.
+		// General.
 
-	subtle    = adaptive.Color("#D9DCCF", "#383838")
-	highlight = adaptive.Color("#874BFD", "#7D56F4")
-	special   = adaptive.Color("#43BF6D", "#73F59F")
+		subtle    = adaptive.Color("#D9DCCF", "#383838")
+		highlight = adaptive.Color("#874BFD", "#7D56F4")
+		special   = adaptive.Color("#43BF6D", "#73F59F")
 
-	divider = lipgloss.NewStyle().
-		SetString("•").
-		Padding(0, 1).
-		Foreground(subtle).
-		String()
-
-	url = lipgloss.NewStyle().Foreground(special).Render
-
-	// Tabs.
-
-	activeTabBorder = lipgloss.Border{
-		Top:         "─",
-		Bottom:      " ",
-		Left:        "│",
-		Right:       "│",
-		TopLeft:     "╭",
-		TopRight:    "╮",
-		BottomLeft:  "┘",
-		BottomRight: "└",
-	}
-
-	tabBorder = lipgloss.Border{
-		Top:         "─",
-		Bottom:      "─",
-		Left:        "│",
-		Right:       "│",
-		TopLeft:     "╭",
-		TopRight:    "╮",
-		BottomLeft:  "┴",
-		BottomRight: "┴",
-	}
-
-	tab = lipgloss.NewStyle().
-		Border(tabBorder, true).
-		BorderForeground(highlight).
-		Padding(0, 1)
-
-	activeTab = tab.Border(activeTabBorder, true)
-
-	tabGap = tab.
-		BorderTop(false).
-		BorderLeft(false).
-		BorderRight(false)
-
-	// Title.
-
-	titleStyle = lipgloss.NewStyle().
-			MarginLeft(1).
-			MarginRight(5).
+		divider = lipgloss.NewStyle().
+			SetString("•").
 			Padding(0, 1).
-			Italic(true).
-			Foreground(lipgloss.Color("#FFF7DB")).
-			SetString("Lip Gloss")
-
-	descStyle = lipgloss.NewStyle().MarginTop(1)
-
-	infoStyle = lipgloss.NewStyle().
-			BorderStyle(lipgloss.NormalBorder()).
-			BorderTop(true).
-			BorderForeground(subtle)
-
-	// Dialog.
-
-	dialogBoxStyle = lipgloss.NewStyle().
-			Border(lipgloss.RoundedBorder()).
-			BorderForeground(lipgloss.Color("#874BFD")).
-			Padding(1, 0).
-			BorderTop(true).
-			BorderLeft(true).
-			BorderRight(true).
-			BorderBottom(true)
-
-	buttonStyle = lipgloss.NewStyle().
-			Foreground(lipgloss.Color("#FFF7DB")).
-			Background(lipgloss.Color("#888B7E")).
-			Padding(0, 3).
-			MarginTop(1)
-
-	activeButtonStyle = buttonStyle.
-				Foreground(lipgloss.Color("#FFF7DB")).
-				Background(lipgloss.Color("#F25D94")).
-				MarginRight(2).
-				Underline(true)
-
-	// List.
-
-	list = lipgloss.NewStyle().
-		Border(lipgloss.NormalBorder(), false, true, false, false).
-		BorderForeground(subtle).
-		MarginRight(2).
-		Height(8).
-		Width(columnWidth + 1)
-
-	listHeader = lipgloss.NewStyle().
-			BorderStyle(lipgloss.NormalBorder()).
-			BorderBottom(true).
-			BorderForeground(subtle).
-			MarginRight(2).
-			Render
-
-	listItem = lipgloss.NewStyle().PaddingLeft(2).Render
-
-	checkMark = lipgloss.NewStyle().SetString("✓").
-			Foreground(special).
-			PaddingRight(1).
+			Foreground(subtle).
 			String()
 
-	listDone = func(s string) string {
-		return checkMark + lipgloss.NewStyle().
-			Strikethrough(true).
-			Foreground(adaptive.Color("#969B86", "#696969")).
-			Render(s)
-	}
+		url = lipgloss.NewStyle().Foreground(special).Render
 
-	// Paragraphs/History.
+		// Tabs.
 
-	historyStyle = lipgloss.NewStyle().
-			Align(lipgloss.Left).
-			Foreground(lipgloss.Color("#FAFAFA")).
-			Background(highlight).
-			Margin(1, 3, 0, 0).
-			Padding(1, 2).
-			Height(19).
-			Width(columnWidth)
+		activeTabBorder = lipgloss.Border{
+			Top:         "─",
+			Bottom:      " ",
+			Left:        "│",
+			Right:       "│",
+			TopLeft:     "╭",
+			TopRight:    "╮",
+			BottomLeft:  "┘",
+			BottomRight: "└",
+		}
 
-	// Status Bar.
+		tabBorder = lipgloss.Border{
+			Top:         "─",
+			Bottom:      "─",
+			Left:        "│",
+			Right:       "│",
+			TopLeft:     "╭",
+			TopRight:    "╮",
+			BottomLeft:  "┴",
+			BottomRight: "┴",
+		}
 
-	statusNugget = lipgloss.NewStyle().
-			Foreground(lipgloss.Color("#FFFDF5")).
+		tab = lipgloss.NewStyle().
+			Border(tabBorder, true).
+			BorderForeground(highlight).
 			Padding(0, 1)
 
-	statusBarStyle = lipgloss.NewStyle().
-			Foreground(adaptive.Color("#343433", "#C1C6B2")).
-			Background(adaptive.Color("#D9DCCF", "#353533"))
+		activeTab = tab.Border(activeTabBorder, true)
 
-	statusStyle = lipgloss.NewStyle().
-			Inherit(statusBarStyle).
-			Foreground(lipgloss.Color("#FFFDF5")).
-			Background(lipgloss.Color("#FF5F87")).
-			Padding(0, 1).
-			MarginRight(1)
+		tabGap = tab.
+			BorderTop(false).
+			BorderLeft(false).
+			BorderRight(false)
 
-	encodingStyle = statusNugget.
-			Background(lipgloss.Color("#A550DF")).
-			Align(lipgloss.Right)
+		// Title.
 
-	statusText = lipgloss.NewStyle().Inherit(statusBarStyle)
+		titleStyle = lipgloss.NewStyle().
+				MarginLeft(1).
+				MarginRight(5).
+				Padding(0, 1).
+				Italic(true).
+				Foreground(lipgloss.Color("#FFF7DB")).
+				SetString("Lip Gloss")
 
-	fishCakeStyle = statusNugget.Background(lipgloss.Color("#6124DF"))
+		descStyle = lipgloss.NewStyle().MarginTop(1)
 
-	// Page.
+		infoStyle = lipgloss.NewStyle().
+				BorderStyle(lipgloss.NormalBorder()).
+				BorderTop(true).
+				BorderForeground(subtle)
 
-	docStyle = lipgloss.NewStyle().Padding(1, 2, 1, 2)
-)
+		// Dialog.
 
-func main() {
+		dialogBoxStyle = lipgloss.NewStyle().
+				Border(lipgloss.RoundedBorder()).
+				BorderForeground(lipgloss.Color("#874BFD")).
+				Padding(1, 0).
+				BorderTop(true).
+				BorderLeft(true).
+				BorderRight(true).
+				BorderBottom(true)
+
+		buttonStyle = lipgloss.NewStyle().
+				Foreground(lipgloss.Color("#FFF7DB")).
+				Background(lipgloss.Color("#888B7E")).
+				Padding(0, 3).
+				MarginTop(1)
+
+		activeButtonStyle = buttonStyle.
+					Foreground(lipgloss.Color("#FFF7DB")).
+					Background(lipgloss.Color("#F25D94")).
+					MarginRight(2).
+					Underline(true)
+
+		// List.
+
+		list = lipgloss.NewStyle().
+			Border(lipgloss.NormalBorder(), false, true, false, false).
+			BorderForeground(subtle).
+			MarginRight(2).
+			Height(8).
+			Width(columnWidth + 1)
+
+		listHeader = lipgloss.NewStyle().
+				BorderStyle(lipgloss.NormalBorder()).
+				BorderBottom(true).
+				BorderForeground(subtle).
+				MarginRight(2).
+				Render
+
+		listItem = lipgloss.NewStyle().PaddingLeft(2).Render
+
+		checkMark = lipgloss.NewStyle().SetString("✓").
+				Foreground(special).
+				PaddingRight(1).
+				String()
+
+		listDone = func(s string) string {
+			return checkMark + lipgloss.NewStyle().
+				Strikethrough(true).
+				Foreground(adaptive.Color("#969B86", "#696969")).
+				Render(s)
+		}
+
+		// Paragraphs/History.
+
+		historyStyle = lipgloss.NewStyle().
+				Align(lipgloss.Left).
+				Foreground(lipgloss.Color("#FAFAFA")).
+				Background(highlight).
+				Margin(1, 3, 0, 0).
+				Padding(1, 2).
+				Height(19).
+				Width(columnWidth)
+
+		// Status Bar.
+
+		statusNugget = lipgloss.NewStyle().
+				Foreground(lipgloss.Color("#FFFDF5")).
+				Padding(0, 1)
+
+		statusBarStyle = lipgloss.NewStyle().
+				Foreground(adaptive.Color("#343433", "#C1C6B2")).
+				Background(adaptive.Color("#D9DCCF", "#353533"))
+
+		statusStyle = lipgloss.NewStyle().
+				Inherit(statusBarStyle).
+				Foreground(lipgloss.Color("#FFFDF5")).
+				Background(lipgloss.Color("#FF5F87")).
+				Padding(0, 1).
+				MarginRight(1)
+
+		encodingStyle = statusNugget.
+				Background(lipgloss.Color("#A550DF")).
+				Align(lipgloss.Right)
+
+		statusText = lipgloss.NewStyle().Inherit(statusBarStyle)
+
+		fishCakeStyle = statusNugget.Background(lipgloss.Color("#6124DF"))
+
+		// Page.
+
+		docStyle = lipgloss.NewStyle().Padding(1, 2, 1, 2)
+	)
+
 	physicalWidth, _, _ := term.GetSize(os.Stdout.Fd())
 	doc := strings.Builder{}
 
-	// Tabs
+	// Tabs.
 	{
 		row := lipgloss.JoinHorizontal(
 			lipgloss.Top,
@@ -228,7 +232,7 @@ func main() {
 		doc.WriteString(row + "\n\n")
 	}
 
-	// Title
+	// Title.
 	{
 		var (
 			colors = colorGrid(1, 5)
@@ -253,7 +257,7 @@ func main() {
 		doc.WriteString(row + "\n\n")
 	}
 
-	// Dialog
+	// Dialog.
 	{
 		okButton := activeButtonStyle.Render("Yes")
 		cancelButton := buttonStyle.Render("Maybe")
@@ -272,7 +276,7 @@ func main() {
 		doc.WriteString(dialog + "\n\n")
 	}
 
-	// Color grid
+	// Color grid.
 	colors := func() string {
 		colors := colorGrid(14, 8)
 
@@ -313,7 +317,7 @@ func main() {
 
 	doc.WriteString(lipgloss.JoinHorizontal(lipgloss.Top, lists, colors))
 
-	// Marmalade history
+	// Marmalade history.
 	{
 		const (
 			historyA = "The Romans learned from the Greeks that quinces slowly cooked with honey would “set” when cool. The Apicius gives a recipe for preserving whole quinces, stems and leaves attached, in a bath of honey diluted with defrutum: Roman marmalade. Preserves of quince and lemon appear (along with rose, apple, plum and pear) in the Book of ceremonies of the Byzantine Emperor Constantine VII Porphyrogennetos."
@@ -331,16 +335,16 @@ func main() {
 		doc.WriteString("\n\n")
 	}
 
-	// Status bar
+	// Status bar.
 	{
 		w := lipgloss.Width
 
-		lightDarkState := func(v bool) string {
-			if v {
+		lightDarkState := func() string {
+			if hasDarkBG {
 				return "Dark"
 			}
 			return "Light"
-		}(hasDarkBG)
+		}()
 
 		statusKey := statusStyle.Render("STATUS")
 		encoding := encodingStyle.Render("UTF-8")
