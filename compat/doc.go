eaab1e7c76634923090271b3c8b4470319b9a4bd
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-11-04 17:04:32 -0500

    docs: rename impure to compat

--- compat/doc.go
+++ compat/doc.go
@@ -1,4 +1,4 @@
-// Package impure is a compatibility layer for Lip Gloss that provides a way to
+// Package compat is a compatibility layer for Lip Gloss that provides a way to
 // deal with the hassle of setting up a writer. It's impure because it uses
 // global variables, is not thread-safe, and only works with the default
 // standard I/O streams.
