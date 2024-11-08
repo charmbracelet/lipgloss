dee5c67839f580f04b92184ac31f5b5995e468cd
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-11-07 21:47:12 -0500

    fix: manually query terminal for background color
    This drops the dependency on `github.com/charmbracelet/x/input` and
    `github.com/erikgeiser/coninput` by manually parsing the terminal
    response to the background color query using `ansi`.
    
    Since we moved `x/input` to Bubble Tea, and the code is no longer
    maintained, we need a different approach to query the terminal for the
    background color. This is basically doing the same thing as before, but
    manually parsing the response instead of using the `x/input` package.
    
    Fixes: https://github.com/charmbracelet/x/issues/248
    Supersedes: https://github.com/charmbracelet/x/pull/249

--- terminal.go
+++ terminal.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"image/color"
 	"io"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/charmbracelet/x/ansi"
-	"github.com/charmbracelet/x/input"
+	"github.com/charmbracelet/x/ansi/parser"
+	"github.com/muesli/cancelreader"
 )
 
 // queryBackgroundColor queries the terminal for the background color.
@@ -24,13 +27,20 @@ import (
 func queryBackgroundColor(in io.Reader, out io.Writer) (c color.Color, err error) {
 	//nolint: errcheck
 	err = queryTerminal(in, out, defaultQueryTimeout,
-		func(events []input.Event) bool {
-			for _, e := range events {
-				switch e := e.(type) {
-				case input.BackgroundColorEvent:
-					c = e.Color
-					continue // we need to consume the next DA1 event
-				case input.PrimaryDeviceAttributesEvent:
+		func(seq string, pa *ansi.Parser) bool {
+			switch {
+			case ansi.HasOscPrefix(seq):
+				switch pa.Cmd {
+				case 11: // OSC 11
+					parts := strings.Split(string(pa.Data[:pa.DataLen]), ";")
+					if len(parts) != 2 {
+						break // invalid, but we still need to parse the next sequence
+					}
+					c = xParseColor(parts[1])
+				}
+			case ansi.HasCsiPrefix(seq):
+				switch pa.Cmd {
+				case 'c' | '?'<<parser.MarkerShift: // DA1
 					return false
 				}
 			}
@@ -44,7 +54,7 @@ const defaultQueryTimeout = time.Second * 2
 // queryTerminalFilter is a function that filters input events using a type
 // switch. If false is returned, the QueryTerminal function will stop reading
 // input.
-type queryTerminalFilter func(events []input.Event) bool
+type queryTerminalFilter func(seq string, pa *ansi.Parser) bool
 
 // queryTerminal queries the terminal for support of various features and
 // returns a list of response events.
@@ -60,9 +70,9 @@ func queryTerminal(
 	filter queryTerminalFilter,
 	query string,
 ) error {
-	rd, err := input.NewDriver(in, "", 0)
+	rd, err := cancelreader.NewReader(in)
 	if err != nil {
-		return fmt.Errorf("could not create driver: %w", err)
+		return fmt.Errorf("could not create cancel reader: %w", err)
 	}
 
 	defer rd.Close() //nolint: errcheck
@@ -81,16 +91,63 @@ func queryTerminal(
 		return fmt.Errorf("could not write query: %w", err)
 	}
 
+	pa := ansi.GetParser()
+	defer ansi.PutParser(pa)
+
+	var buf [256]byte // 256 bytes should be enough for most responses
 	for {
-		events, err := rd.ReadEvents()
+		n, err := rd.Read(buf[:])
 		if err != nil {
-			return fmt.Errorf("could not read events: %s", err)
+			return fmt.Errorf("could not read from input: %w", err)
 		}
 
-		if !filter(events) {
-			break
+		var state byte
+		p := buf[:]
+		for n > 0 {
+			seq, _, read, newState := ansi.DecodeSequence(p[:n], state, pa)
+			if !filter(string(seq), pa) {
+				return nil
+			}
+
+			state = newState
+			n -= read
+			p = p[read:]
 		}
 	}
+}
 
+func shift(x uint64) uint64 {
+	if x > 0xff {
+		x >>= 8
+	}
+	return x
+}
+
+func xParseColor(s string) color.Color {
+	switch {
+	case strings.HasPrefix(s, "rgb:"):
+		parts := strings.Split(s[4:], "/")
+		if len(parts) != 3 {
+			return color.Black
+		}
+
+		r, _ := strconv.ParseUint(parts[0], 16, 32)
+		g, _ := strconv.ParseUint(parts[1], 16, 32)
+		b, _ := strconv.ParseUint(parts[2], 16, 32)
+
+		return color.RGBA{uint8(shift(r)), uint8(shift(g)), uint8(shift(b)), 255} //nolint:gosec
+	case strings.HasPrefix(s, "rgba:"):
+		parts := strings.Split(s[5:], "/")
+		if len(parts) != 4 {
+			return color.Black
+		}
+
+		r, _ := strconv.ParseUint(parts[0], 16, 32)
+		g, _ := strconv.ParseUint(parts[1], 16, 32)
+		b, _ := strconv.ParseUint(parts[2], 16, 32)
+		a, _ := strconv.ParseUint(parts[3], 16, 32)
+
+		return color.RGBA{uint8(shift(r)), uint8(shift(g)), uint8(shift(b)), uint8(shift(a))} //nolint:gosec
+	}
 	return nil
 }
