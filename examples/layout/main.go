7420be022b7b636390c65ce8c0a10f8bf8279a8d
bashbunni <bunni@bashbunni.dev>
2024-12-02 15:42:12 -0800

    fixup! merge v2-exp

--- examples/layout/main.go
+++ examples/layout/main.go
@@ -142,9 +142,9 @@ func main() {
 		list = lipgloss.NewStyle().
 			Border(lipgloss.NormalBorder(), false, true, false, false).
 			BorderForeground(subtle).
-			MarginRight(2).
+			MarginRight(1).
 			Height(8).
-			Width(columnWidth + 1)
+			Width(width / 3)
 
 		listHeader = lipgloss.NewStyle().
 				BorderStyle(lipgloss.NormalBorder()).
