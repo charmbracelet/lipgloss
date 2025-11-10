c95c5f3c5b27360d344bf82736a8ce9257aaf71e
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-11-10 15:58:14 -0500

    feat: add hyperlink support (#473)

--- style.go
+++ style.go
@@ -83,6 +83,10 @@ const (
 	tabWidthKey
 
 	transformKey
+
+	// Hyperlink.
+	linkKey
+	linkParamsKey
 )
 
 // props is a set of properties.
@@ -114,6 +118,9 @@ func NewStyle() Style {
 type Style struct {
 	props props
 	value string
+
+	// hyperlink
+	link, linkParams string
 
 	// we store bool props values here
 	attrs int
@@ -283,6 +290,8 @@ func (s Style) Render(strs ...string) string {
 		useSpaceStyler = (underline && !underlineSpaces) || (strikethrough && !strikethroughSpaces) || underlineSpaces || strikethroughSpaces
 
 		transform = s.getAsTransform(transformKey)
+
+		link, linkParams = s.GetHyperlink()
 	)
 
 	if transform != nil {
@@ -393,6 +402,10 @@ func (s Style) Render(strs ...string) string {
 		}
 
 		str = b.String()
+
+		if len(link) > 0 {
+			str = ansi.SetHyperlink(link, linkParams) + str + ansi.ResetHyperlink()
+		}
 	}
 
 	// Padding
